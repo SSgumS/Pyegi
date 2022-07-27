f453bf95412ea5493d14f17e2f061a374dc02de5
SSgumS <sssafais@hotmail.com>
2022-07-27 16:25:41 +0200

    some cleanups fix changing windows size when you first show a preview fix layout on settings optimize copy2aegisub task

--- tasks/steps/copy2aegisub.py
+++ tasks/steps/copy2aegisub.py
@@ -2,8 +2,18 @@ import logging
 import os
 import shutil
 import appdirs
+import time
+from typing import Union
 
 logger = logging.getLogger(__name__)
+
+
+def get_last_modified(path: str, in_float: bool = False) -> Union[str, float]:
+    last_modified = max(os.path.getmtime(root) for root, _, _ in os.walk(".venv"))
+    if in_float:
+        return last_modified
+    else:
+        return time.ctime(last_modified)
 
 
 def run_step(context: dict):
@@ -14,11 +24,18 @@ def run_step(context: dict):
 
     # Remove previous files
     shutil.rmtree(os.path.join(automation_path, "include", "Pyegi"), ignore_errors=True)
-    shutil.rmtree(os.path.join(pyegi_dependency_path, ".venv"), ignore_errors=True)
     shutil.rmtree(os.path.join(pyegi_dependency_path, "Pyegi"), ignore_errors=True)
-    shutil.rmtree(os.path.join(pyegi_dependency_path, "PythonScripts"), ignore_errors=True)
+    shutil.rmtree(
+        os.path.join(pyegi_dependency_path, "PythonScripts"), ignore_errors=True
+    )
 
     # Copy new files
     shutil.copytree("src", automation_path, dirs_exist_ok=True)
     shutil.copy("poetry.toml", pyegi_dependency_path)
     shutil.copy("pyproject.toml", pyegi_dependency_path)
+
+    # Copy .venv
+    target_venv_path = os.path.join(pyegi_dependency_path, ".venv")
+    if get_last_modified(".venv", True) != get_last_modified(target_venv_path, True):
+        shutil.rmtree(target_venv_path, ignore_errors=True)
+        shutil.copytree(".venv", target_venv_path, dirs_exist_ok=True)
